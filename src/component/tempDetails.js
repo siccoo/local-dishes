50e4e4b588da5eef0fce0fe575189c937ce7cb09
siccoo <obichilaka@gmail.com>
2019-10-08 17:03:27 +0100

    first commit to local nigerian dishes and how it is cooked

---
+++ src/component/tempDetails.js
@@ -0,0 +1,23 @@
+export const recipe = {
+    ngn_url: "https://www.allnigerianrecipes.com/soups/",
+    image_url: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/rivers-native-soup.jpg",
+    ingerdients: [
+        "3 medium fresh fish",
+        "9 snails",
+        "2 cups clams",
+        "1 cup sea snails",
+        "½ cup periwinkles",
+        "1 cup shrimps",
+        "6 medium cocoyams",
+        "1 tablespoon uziza seeds (false cubeb)",
+        "1-2 yellow habanero peppers",
+        "2 big stock cubes",
+        "Bitter leaves (washed)",
+        "5 Uziza leaves (false kubeb)",
+        "2 cooking spoons (¼ cup) red palm oil"
+    ],
+    publisher: "All Nigerian Recipes",
+    publisher_url: "https://www.allnigerianrecipes.com",
+    source_url: "https://www.allnigerianrecipes.com/soups/rivers-native-soup/",
+    title: "Rivers Native Soup"
+};
---
+++ src/component/tempList.js
@@ -0,0 +1,49 @@
+export const recipes = [
+    {
+        recipe_id: 1,
+        image_url: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/rivers-native-soup.jpg",
+        title: "Rivers Native Soup",
+        publisher: "All Nigerian Recipes",
+        source_url: "https://www.allnigerianrecipes.com/soups/rivers-native-soup/"
+    },
+
+    {
+        recipe_id: 2,
+        image_url: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/rivers-native-soup.jpg",
+        title: "Rivers Native Soup",
+        publisher: "All Nigerian Recipes",
+        source_url: "https://www.allnigerianrecipes.com/soups/rivers-native-soup/"
+    },
+
+    {
+        recipe_id: 3,
+        image_url: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/rivers-native-soup.jpg",
+        title: "Rivers Native Soup",
+        publisher: "All Nigerian Recipes",
+        source_url: "https://www.allnigerianrecipes.com/soups/rivers-native-soup/"
+    },
+
+    {
+        recipe_id: 4,
+        image_url: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/rivers-native-soup.jpg",
+        title: "Rivers Native Soup",
+        publisher: "All Nigerian Recipes",
+        source_url: "https://www.allnigerianrecipes.com/soups/rivers-native-soup/"
+    },
+
+    {
+        recipe_id: 5,
+        image_url: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/rivers-native-soup.jpg",
+        title: "Rivers Native Soup",
+        publisher: "All Nigerian Recipes",
+        source_url: "https://www.allnigerianrecipes.com/soups/rivers-native-soup/"
+    },
+
+    {
+        recipe_id: 6,
+        image_url: "https://www.allnigerianrecipes.com/wp-content/uploads/2019/04/rivers-native-soup.jpg",
+        title: "Rivers Native Soup",
+        publisher: "All Nigerian Recipes",
+        source_url: "https://www.allnigerianrecipes.com/soups/rivers-native-soup/"
+    },
+]
